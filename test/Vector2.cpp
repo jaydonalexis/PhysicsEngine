e448b7101add8ef17cfc54115b63e7d741495bf1
Jaydon Alexis <jjalexis@student.ubc.ca>
2023-08-26 20:06:28 -0700

    Setting up testing

---
+++ test/Vector2.cpp
@@ -0,0 +1,10 @@
+#include "UnitTests.h"
+
+#include <physics/mathematics/Vector2.h>
+
+#include <sstream>
+#include <type_traits>
+#include <algorithm>
+#include <cstddef>
+
+using namespace physics;
