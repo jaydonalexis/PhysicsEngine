101f02c164f33d1726bea084e3056faab31c21a4
Jaydon Alexis <jjalexis@student.ubc.ca>
2023-09-20 00:05:28 -0700

    Added header for free list allocator

--- include/physics/memory/FreeList.h
+++ include/physics/memory/FreeList.h
@@ -9,6 +9,83 @@ namespace physics {
 
 class FreeListMemoryHandler : public MemoryHandler {
 
+  private:
+    /* -- Nested Classes -- */
+
+    /* Allocation headers are used to record the size of the memory block needed to be freed when deallocating */
+    struct AllocationHeader {
+
+      public:
+        /* -- Attributes -- */
+
+        /* Size of allocated memory block */
+        size_t size;
+
+        /* Memory block's allocation status */
+        bool isAllocated;
+
+        /* Next memory block in list */
+        AllocationHeader* next;
+
+        /* Previous memory block in list */
+        AllocationHeader* prev;
+
+        /* Next memory block in the list is a valid candidate for coalescence */
+        bool isNextCoalescent;
+
+        /* -- Methods -- */
+
+        /* Constructor */
+        AllocationHeader(size_t size, AllocationHeader* prev, AllocationHeader* next, bool isNextCoalescent) : size(size), isAllocated(false), prev(prev), next(next), isNextCoalescent(isNextCoalescent) {
+          assert(size > 0);
+        }
+    };
+
+    /* -- Attributes -- */
+
+    /* Initial size */
+    static size_t INIT_SIZE;
+
+    /* Mutex to prevent simultaneous allocations */
+    std::mutex mMutex;
+
+    /* Primary memory handler */
+    MemoryHandler& mPrimaryMemoryHandler;
+
+    /* Total size of allocated memory */
+    size_t mAllocated;
+
+    /* First block of the linked list */
+    AllocationHeader* mHead;
+
+    /* Free memory block */
+    AllocationHeader* mFree;
+
+    /* -- Methods -- */
+    
+    /* Split memory block into two portions */
+    void split(AllocationHeader* block, size_t size);
+
+    /* Coalesce two blocks */
+    void coalesce(AllocationHeader* block1, AllocationHeader* block2);
+
+    /* Reserve extra memory */
+    void reserve(size_t size);
+
+  public:
+    /* -- Methods -- */
+
+    /* Constructor */
+    FreeListMemoryHandler(MemoryHandler& primaryMemoryHandler, size_t initSize = 0);
+
+    /* Destructor */
+    ~FreeListMemoryHandler() override;
+
+    /* Dynamically allocate memory of size in bytes and return a pointer to the heap allocated block */
+    void* allocate(size_t size) override;
+
+    /* Free dynamically allocated memory */
+    void free(void* ptr, size_t size) override;
 };
 
 }
