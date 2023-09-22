65adc4dd19f075c57dc3653e3ea05dc3f6949912
Jaydon Alexis <jjalexis@student.ubc.ca>
2023-09-22 12:55:15 -0700

    Finished implementing free list allocator

--- src/memory/FreeList.cpp
+++ src/memory/FreeList.cpp
@@ -33,7 +33,7 @@ void* FreeListMemoryHandler::allocate(size_t size) {
   std::lock_guard<std::mutex> lock(mMutex);
   assert(size > 0);
 
-  /* Not possible to allocated zero memory */
+  /* Not possible to allocate zero memory */
   if(size == 0) {
     return nullptr;
   }
@@ -45,7 +45,7 @@ void* FreeListMemoryHandler::allocate(size_t size) {
   if(mFree) {
     assert(!mFree->isAllocated);
 
-    /* Can the block accomodate the size request */
+    /* Can the block accomodate the allocation request */
     if(size <= mFree->size) {
       block = mFree;
       mFree = nullptr;
@@ -53,7 +53,9 @@ void* FreeListMemoryHandler::allocate(size_t size) {
   }
 
   while(block) {
+    /* Block can accomodate the allocation request */
     if(!block->isAllocated && size <= block->size) {
+      /* Split memory block into two portions */
       split(block, size);
       break;
     }
@@ -61,10 +63,12 @@ void* FreeListMemoryHandler::allocate(size_t size) {
     block = block->next;
   }
 
+  /* More memory required */
   if(!block) {
     apportion((mAllocated + size) * 2);
     assert(mFree);
     assert(!mFree->isAllocated);
+    /* Request for extra memory has been fulfilled */
     block = mFree;
     assert(block->size >= size);
     split(block, size);
@@ -77,10 +81,40 @@ void* FreeListMemoryHandler::allocate(size_t size) {
     mFree = block->next;
   }
 
+  /* Void pointer to the memory space of the block after the allocation header */
   return static_cast<void*>(reinterpret_cast<unsigned char*>(block) + sizeof(AllocationHeader));
 }
 
 /* Free dynamically allocated memory */
+void FreeListMemoryHandler::free(void* ptr, size_t size) {
+  std::lock_guard<std::mutex> lock(mMutex);
+  assert(size > 0);
+  
+  /* Not possible to free zero memory */
+  if(size == 0) {
+    return;
+  }
+
+  unsigned char* blockAddress = static_cast<unsigned char*>(ptr) - sizeof(AllocationHeader);
+  AllocationHeader* block = reinterpret_cast<AllocationHeader*>(blockAddress);
+  assert(block->isAllocated);
+  block->isAllocated = false;
+
+  AllocationHeader* tempBlock = block;
+
+  /* Merge previous block with current block if contiguous in memory and the previous block is not allocated */
+  if(block->prev && !block->prev->isAllocated && block->prev->isNextCoalescent) {
+    tempBlock = block->prev;
+    coalesce(block->prev, block);
+  }
+  
+  /* Merge next block with current block is contiguous in memory and the next block is not allocated */
+  if(tempBlock->next && !tempBlock->next->isAllocated && tempBlock->isNextCoalescent) {
+    coalesce(tempBlock, tempBlock->next);
+  }
+
+  mFree = tempBlock;
+}
 
 /* Split memory block into two portions */
 void FreeListMemoryHandler::split(AllocationHeader* block, size_t size) {
@@ -88,6 +122,7 @@ void FreeListMemoryHandler::split(AllocationHeader* block, size_t size) {
   assert(!block->isAllocated);
 
   if(size + sizeof(AllocationHeader) < block->size - sizeof(AllocationHeader)) {
+    /* New memory block for the leftover space */
     unsigned char* nextBlockAddress = reinterpret_cast<unsigned char*>(block) + sizeof(AllocationHeader) + size;
     AllocationHeader* newBlock = new (static_cast<void*>(nextBlockAddress)) AllocationHeader(block->size - (2 * sizeof(AllocationHeader)) - size, block, block->next, block->isNextCoalescent);
     assert(newBlock->next != newBlock);
@@ -107,17 +142,42 @@ void FreeListMemoryHandler::split(AllocationHeader* block, size_t size) {
   }
 }
 
+/* Coalesce two blocks */
+void FreeListMemoryHandler::coalesce(AllocationHeader* block1, AllocationHeader* block2) {
+  assert(block1->next == block2);
+  assert(block2->prev == block1);
+  assert(!block1->isAllocated);
+  assert(!block2->isAllocated);
+  assert(block1->isNextCoalescent);
+
+  block1->size += sizeof(AllocationHeader) + block2->size;
+  block1->next = block2->next;
+  assert(block1->next != block1);
+
+  if(block2->next) {
+    block2->next->prev = block1;
+  }
+
+  block1->isNextCoalescent = block2->isNextCoalescent;
+  block2->~AllocationHeader();
+  assert(!block1->prev || block1->prev->next == block1);
+  assert(!block1->next || block1->next->prev == block1);
+}
+
 /* Apportion additional memory */
 void FreeListMemoryHandler::apportion(size_t size) {
+  /* Call on primary memory handler to allocate new memory */
   void* raw = mPrimaryMemoryHandler.allocate(size + sizeof(AllocationHeader));
   assert(raw);
 
+  /* New header for our allocated memory */
   AllocationHeader* block = new (raw) AllocationHeader(size, nullptr, mHead, false);
 
   if(mHead) {
     mHead->prev = block;
   }
 
+  /* Allocated memory added to the beginning of the linked list */
   mHead = block;
   mFree = mHead;
   mAllocated += size;
