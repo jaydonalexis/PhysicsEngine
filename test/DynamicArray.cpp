dfbaae7aee8db7a5835cf6c28c2f4951f388d8d4
Jaydon Alexis <jjalexis@student.ubc.ca>
2023-09-27 12:00:54 -0700

    Added test files for dynamic array and pair data structures

---
+++ test/DynamicArray.cpp
@@ -0,0 +1,10 @@
+#include "UnitTests.h"
+
+#include <physics/collectors/DynamicArray.h>
+
+#include <sstream>
+#include <type_traits>
+#include <algorithm>
+#include <cstddef>
+
+using namespace physics;
---
+++ test/Pair.cpp
@@ -0,0 +1,10 @@
+#include "UnitTests.h"
+
+#include <physics/collectors/Pair.h>
+
+#include <sstream>
+#include <type_traits>
+#include <algorithm>
+#include <cstddef>
+
+using namespace physics;
